0956e98bf58339165a4b3101413835bcb68f2af0
Daniel AL-Abdrabbuh <maa.alabdrabbuh@gmail.com>
2012-03-21 15:05:04 -0400

    second commit

---
+++ change_grade_module.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import re
+import fileinput
+from sys import stderr,stdout,argv,exit
+
+arg1 = argv[1]
+arg2 = argv[2]
+
+def change_grade():
+	if len(argv) != 3:
+		    stderr.write("Usage: change_grade.py  STRING\n")
+		    exit(1)
+		    
+		for line in fileinput.input('-'):
+		 	m = re.search(argv[1], line)
+		 	values = line.rstrip().rsplit(',') #note that split accepts an optional parameter to define what character the string is split on. If none is provided it defaults to any whitespace character  	
+			if m:
+		       		values[3] = argv[2]
+				print ",".join(values)	
+			else:
+				print line	
+
+## This should be a module later on
+def main():
+	change_grade()	
+
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
